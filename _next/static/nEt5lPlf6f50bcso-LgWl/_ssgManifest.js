794f3c221b7afac941d4d28f4a9a1c3fcf30424c
Christoph Flasch <c.flasch@overlap.at>
2024-02-05 11:18:47 +0100

    export test

---
+++ _next/static/nEt5lPlf6f50bcso-LgWl/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F[...slug]","\u002Fexternal-topbar","\u002Fkontakt","\u002Fmail-attachments","\u002Fmeilensteine","\u002Fvertraulicher-hinweis"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
